d4e90df6c8b586d2d3b5a0645f366a2da04f1c10
Krishna Reddy <kuchalaknati@outlook.com>
2024-03-08 08:15:57 +0530

    added credentials

--- provider.tf
+++ provider.tf
@@ -1,5 +1,9 @@
 provider "azurerm" {
-  features {
+  features {}
     
-  }
+  client_id       = "e0c282de-1aac-4bad-846f-cc6d5ad00ff3"
+  client_secret   = "ZEw8Q~Ofe3zsigINJgyV_4yGZrs2ppSph4LHMcdz"
+  tenant_id       = "06e35325-7e4a-4dbc-9be6-1c1c89bc2c1a"
+  subscription_id = "adfed678-4682-4bb0-a62f-2ebd77f373fd"
+  
 }
